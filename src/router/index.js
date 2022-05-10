0109b2ae5f9417fce2593cb0006458ec8cfa3da1
venson <venson.hunt@gmail.com>
2022-05-10 18:26:02 +0800

    [feature] add subject

--- src/router/index.js
+++ src/router/index.js
@@ -85,6 +85,28 @@ export const constantRoutes = [
       }
     ],
   },
+
+  {
+    path: "/subject",
+    component: Layout,
+    redirect: "/subject/list",
+    name: "课程管理",
+    meta: { title: "课程管理", icon: "el-icon-s-help" },
+    children: [
+      {
+        path: "list",
+        name: "课程列表",
+        component: () => import("@/views/edu/subject/list"),
+        meta: { title: "课程列表",icon: "table" },
+      },
+      {
+        path: "save",
+        name: "添加课程",
+        component: () => import("@/views/edu/subject/save"),
+        meta: { title: "添加课程", icon: "tree" },
+      },
+    ],
+  },
   {
     path: "/example",
     component: Layout,
---
+++ src/views/edu/subject/save.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="app-container">
+    <el-form label-width="120px">
+      <el-form-item label="信息描述">
+        <el-tag type="info">excel模板说明</el-tag>
+
+        <el-tag>
+          <i class="el-icon-download" />
+          <a :href="'/static/01.xlsx'">模板下载</a>
+        </el-tag>
+      </el-form-item>
+      <el-form-item label="文件上传">
+        <el-upload
+          ref="upload"
+          :action= "BASE_API+'eduservice/edu-subject/addSubject'"
+          name="file"
+          accept=".xls, .xlsx"
+          :on-success="fileUploadSuccess"
+          :on-error="fileUploadError"
+          :disabled="importBtnDisabled"
+          :limit="1"
+          :auto-upload="false"
+        >
+          <el-button slot="trigger" size="small" type="primary"
+            >选择文件</el-button
+          >
+          <el-button
+            style="margin-left: 10px"
+            size="small"
+            type="success"
+            @click="submitUpload"
+            :loading="loading"
+            >上传到服务器</el-button
+          >
+        </el-upload>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      importBtnDisabled: false,
+      BASE_API: process.env.VUE_APP_BASE_API,
+      loading: false
+
+    };
+  },
+  methods: {
+    submitUpload(){
+      console.log(this.BASE_API)
+      this.importBtnDisabled=true
+      this.loading=true
+      this.$refs.upload.submit()
+
+    },
+    fileUploadSuccess(){
+      this.loading = false
+      this.$message({
+        type: 'success',
+        message: '添加成功'
+      })
+
+    },
+    fileUploadError(){
+      this.loading = false
+      this.$message({
+        type: 'error',
+        message: '添加失败'
+      })
+    }
+  },
+};
+</script>
