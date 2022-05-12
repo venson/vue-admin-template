2c8977691f1de48acfa1a5e3e7256bd833e4a3f1
venson <venson.hunt@gmail.com>
2022-05-13 00:27:27 +0800

    [feature] step2 chapter information edit

--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="app-container">
     <h2 style="text-align: center">发布新课程</h2>
-    <el-steps
-      :active="2"
-      process-status="wait"
-      align-center
-      style="margin: bottom 4px"
-      finish-status="success"
-    >
+    <el-steps :active="2" process-status="wait" align-center style="margin: bottom 4px" finish-status="success">
       <el-step title="填写课程基本信息"></el-step>
       <el-step title="创建课程大纲"></el-step>
       <el-step title="最终发布"></el-step>
@@ -15,27 +9,61 @@
 
     <ul>
       <li v-for="chapter in chapterVideoList" :key="chapter.id">
-        {{chapter.title}}
-          <ul>
-            <li v-for="video in chapter.children" :key="video.id">
-            {{video.title}}
-            </li>
-          </ul>
-        </li>
+        {{ chapter.title }}
+        <ul>
+          <li v-for="video in chapter.children" :key="video.id">
+            {{ video.title }}
+          </li>
+        </ul>
+      </li>
     </ul>
 
+    <el-button type="text" @click="dialogChapterFormVisible = true">添加章节</el-button>
+    
+    <!-- 章节 -->
+    <ul class="chapterList" dialogChaptedialo>
+      <li v-for="chapter in chapterNestedList" :key="chapter.id">
+        <p>
+          {{ chapter.title }}
+          <span class="acts">
+            <el-button type="text">添加课时</el-button>
+            <el-button style="" type="text">编辑</el-button>
+            <el-button type="text">删除</el-button>
+          </span>
+        </p>
+        <!-- 视频 -->
+        <ul class="chanpterList videoList">
+          <li v-for="video in chapter.children" :key="video.id">
+            <p>{{ video.title }}
+              <span class="acts">
+                <el-button type="text">编辑</el-button>
+                <el-button type="text">删除</el-button>
+              </span>
+            </p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div>
+      <el-button @click="previous">上一步</el-button>
+      <el-button :disabled="saveBtnDisabled" type="primary" @click="next">下一步</el-button>
+    </div>
 
 
-    <el-form label-width="120px">
-
-      <el-form-item>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="previous"
-          >上一部</el-button>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="next"
-          >下一步</el-button
-        >
-      </el-form-item>
-    </el-form>
+    <el-dialog :visible.sync="dialogChapterFormVisible" title="添加章节">
+      <el-form :model="chapter" label-width="120px">
+        <el-form-item label="章节标题">
+          <el-input v-model="chapter.title" />
+        </el-form-item>
+        <el-form-item label="章节排序">
+          <el-input-number v-model="chapter.sort" :min="0" controls-position="right" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogChapterFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveOrUpdate">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -46,20 +74,24 @@ export default {
     return {
       saveBtnDisabled: false,
       courseId: "",
-      chapterVideoList: []
+      chapterVideoList: [],
+      chapter: {
+
+      },
+      dialogChapterFormVisible: false
     };
   },
   created() {
-    if(this.$route.params && this.$route.params.id){
+    if (this.$route.params && this.$route.params.id) {
       this.courseId = this.$route.params.id
       console.log(this.courseId)
     }
     this.getChapterVideo()
   },
   methods: {
-    getChapterVideo(){
+    getChapterVideo() {
       chapter.getChapterVideo(this.courseId)
-        .then(response =>{
+        .then(response => {
           this.chapterVideoList = response.data.list
         })
     },
